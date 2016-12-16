7c1108b827bf1043e15f540ab3c1f22d4429bb63
softmantk <softmantk@users.noreply.github.com>
2016-12-16 15:29:40 +0530

    qw

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ if(isset($_POST['submit'])){
     $subject = $_POST['subject'];
     $address = $_POST['address'];
     $subject2 = "Copy of your form submission";
-    $message = "Name : ".$name."\n"."Company : ".$company."\n"."Address : ".$address."\n"." wrote the following:"."\n".$message;
+    $message = "Name : ".$name."\n"."Company : ".$company."\n"."Address : ".$address."\n"." wrote the following:"."\n".$_POST['message'];
     $headers = "From:" . $from;
     mail($to,$subject,$message,$headers);
     echo "!!!!!!!!!!!!!!!Mail Senthbh. Thank you " . $_POST['name'] . ", we will contact you shortly.!!!!";
